78046febdc223773328358b5d0c82b6aab6a9f5f
Simo-chan <101993497+Simo-chan@users.noreply.github.com>
2026-03-12 14:40:01 +0200

    Time: 0 ms (100%), Space: 4 MB (50%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.go
@@ -0,0 +1,20 @@
+func scoreOfString(s string) int {
+    var result int
+
+    //We iterate only until the second to last elenemt to avoid going out of bounds.
+    //We convert byte to int right away because byte can hold values only from 0 to 255.
+    // Then we need to find the absolute value, because sometimes substruction results in a 
+    //negative number. If it's the case we substruct negative number from the result variable, 
+    //because double minus equals to plus.
+    for i:= 0; i < len(s)-1; i++ {
+        difference:= int(s[i]) - int(s[i+1])
+
+        if difference > 0 {
+            result+= difference
+        } else {
+            result-= difference
+        }
+    }
+
+    return result
+}
