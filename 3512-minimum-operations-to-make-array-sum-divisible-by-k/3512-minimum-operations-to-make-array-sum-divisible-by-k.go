a339b186d9b6fd4cee4e04a9f94517a43c16a74a
Simo-chan <101993497+Simo-chan@users.noreply.github.com>
2026-03-12 20:52:40 +0200

    Update 3512-minimum-operations-to-make-array-sum-divisible-by-k.go

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.go
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.go
@@ -5,5 +5,9 @@ func minOperations(nums []int, k int) int {
         sum += num
     }
 
+    //Modulo operator will return the remainder which will be exactly the amount of operations needed.
     return sum % k
 }
+
+//Time complexity is O(n)
+//Space complexity is O(1)
