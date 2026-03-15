f0017cf1ebc8eec06a347b5cab10aae762835b8c
Simo-chan <101993497+Simo-chan@users.noreply.github.com>
2026-03-15 22:12:18 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,3 +1,13 @@
 func containsDuplicate(nums []int) bool {
-    
+    uniqueNums := make(map[int]struct{})
+
+    for _, num:= range nums{
+        if _, exists := uniqueNums[num]; !exists {
+            uniqueNums[num] = struct{}{}
+        } else {
+            return true
+        }
+    }
+
+    return false 
 }
